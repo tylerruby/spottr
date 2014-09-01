2bf77aed89fff4ada7c851bec540df492aa24b84
Victor Cortes <ie.cortex@gmail.com>
2014-09-01 17:40:58 -0500

    Show all places in map

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,5 +5,12 @@ class PagesController < ApplicationController
       @lat = coordinates[0]
       @long = coordinates[1]
     end
+
+    @places = Place.all
+    @hash = Gmaps4rails.build_markers(@places) do |place, marker|
+      marker.lat place.latitude
+      marker.lng place.longitude
+      marker.infowindow place.title
+    end
   end
 end
