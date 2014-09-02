f7ba107bbf998e91c804acdf964fd39cf33e1a7b
Victor Cortes <ie.cortex@gmail.com>
2014-09-02 18:54:23 -0500

    Show current city

--- app/helpers/places_helper.rb
+++ app/helpers/places_helper.rb
@@ -3,4 +3,9 @@ module PlacesHelper
   def has_coordinates?
     session[:coordinates].present?
   end
+
+  def current_city
+    results = Geocoder.search(session_coordinates).first
+    "#{results.city}, #{results.state}"
+  end
 end
