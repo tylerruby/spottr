f0f494c5222b34f7c49b3dabe2121c9ca7e6f7cb
Victor Cortes <ie.cortex@gmail.com>
2014-09-02 17:39:57 -0500

    Store place city

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,2 +1,8 @@
 class Place < ActiveRecord::Base
+  reverse_geocoded_by :latitude, :longitude do |obj,results|
+    if geo = results.first
+      obj.city    = geo.city
+    end
+  end
+  after_validation :reverse_geocode  # auto-fetch address
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140901205602) do
+ActiveRecord::Schema.define(version: 20140902223116) do
 
   create_table "places", force: true do |t|
     t.string   "title"
@@ -19,6 +19,9 @@ ActiveRecord::Schema.define(version: 20140901205602) do
     t.float    "longitude"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "ip_address"
+    t.string   "address"
+    t.string   "city"
   end
 
 end
