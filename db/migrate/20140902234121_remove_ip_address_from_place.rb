360352c1ef033af7068d7f2a6e7441fd089c4de7
Tyler Ruby <tylerruby@comcast.net>
2014-09-04 17:13:50 -0400

    More minor changes

--- db/migrate/20140902234121_remove_ip_address_from_place.rb
+++ db/migrate/20140902234121_remove_ip_address_from_place.rb
@@ -1,5 +1,5 @@
 class RemoveIpAddressFromPlace < ActiveRecord::Migration
   def change
-    remove_column :placesgd, :ip_address, :string
+    remove_column :places, :ip_address, :string
   end
 end
