b486d19839245393522638d5dd5bd0948730dc7c
Seva Rybakov <coding.fu@gmail.com>
2014-11-06 19:08:27 +0300

    show place image on map instead of dish image

--- app/models/menu_item.rb
+++ app/models/menu_item.rb
@@ -16,7 +16,8 @@ class MenuItem < ActiveRecord::Base
   def as_json(options={})
     json = super(options)
     json["title"] = self.name
-    json["image_url"] = self.image.url(:thumb)
+    json["preview_image_url"] = self.image.url(:thumb)
+    json["image_url"] = self.place.image.url(:tiny)
     json["place_title"] = self.place.title
     json["latitude"] = self.place.latitude
     json["longitude"] = self.place.longitude
